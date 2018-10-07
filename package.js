6790642af8f43c660acf5b63492d5c568bf2d3bf
stefanocudini <stefano.cudini@gmail.com>
2018-10-08 00:02:15 +0200

    version 1.3.7

--- package.js
+++ package.js
@@ -1,8 +1,7 @@
-
 Package.describe({
+  version: "1.3.7",
   name: "keplerjs:mapillary",
   summary: "Keplerjs mapillary API",
-  version: "1.3.0",
   git: "https://github.com/Keplerjs/keplerjs-mapillary.git"
 });
 
@@ -15,7 +14,7 @@ Package.onUse(function (api) {
   api.versionsFrom("1.5.1");
 
   api.use([
-    'keplerjs:core@1.3.0',
+    'keplerjs:core@1.3.7',
   ]);
 
   api.addFiles([
